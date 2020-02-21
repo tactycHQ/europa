b254db2035db5167338d14c2fdb2b10d5c17f5f8
anubhav13@gmail.com <Evolution2019!>
2020-02-21 12:38:07 -0800

    First commit

--- src/ModeControllers/LandingPage.js
+++ src/ModeControllers/LandingPage.js
@@ -5,6 +5,8 @@ import Button from "@material-ui/core/Button"
 import Main from "./Main";
 import {useAuth0} from "../react-auth0-spa"
 import Background from "../images/home.jpg"
+import {Route, Switch} from "react-router";
+import Dashboard from "../Content/Dashboard";
 
 export default function LandingPage() {
 
@@ -57,81 +59,93 @@ export default function LandingPage() {
             />
         )
     } else {
-        mainEl = (
-            <Paper className={classes.root} square={true}>
-                <div className={classes.mainPage}
-                     style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <div style={{
-                        display: 'flex',
-                        width: '100%',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        padding: '10px'
-                    }}>
-                        <h1 className={classes.logo} style={{
-                            fontSize: '1em',
-                            letterSpacing: '1px',
-                            fontWeight: '500',
-                            margin: '0px',
-                            marginTop: '3px'
-                        }}>How does Flexboard Work?</h1>
-                        <h1 className={classes.logo} style={{
-                            fontSize: '1em',
-                            letterSpacing: '1px',
-                            fontWeight: '500',
-                            margin: '0px',
-                            marginTop: '3px',
-                            justifyContent: 'center'
-                        }}>About Us</h1>
-                    </div>
-
-                    <h6 className={classes.logo}>FLEXBOARD</h6>
-
-                    <h1 className={classes.logo} style={{
-                        fontSize: '1.5em',
-                        letterSpacing: '1px',
-                        fontWeight: '100',
-                        margin: '0px',
-                        marginTop: '5px'
-                    }}>
-                        Powering Enterprise Models to Life
-                        <sup style={{fontSize: '0.7em', marginLeft: '5px'}}>&trade;</sup>
-                    </h1>
-                    <Button
-                        elevation={50}
-                        style={{
-                            marginTop: '50px',
-                            backgroundColor: '#A5014B',
-                            fontFamily: 'Questrial',
-                            fontSize: '1em',
-                            width: '300px',
-                            color: '#E7F1F6'
-                        }}
-                        onClick={() => loginWithRedirect({
-                            redirect_uri: 'http://localhost:3000/home',
-                            // audience:'http://localhost/5000',
-                            // scope:'read:records'
-                        })}>
-                        Secure Log in
-                    </Button>
-                    <Button
-                        elevation={50}
-                        style={{
-                            marginTop: '50px',
-                            backgroundColor: '#006E9F',
-                            fontFamily: 'Questrial',
-                            fontSize: '1em',
-                            width: '300px',
-                            color: '#E7F1F6'
-                        }}>Create an Account</Button>
-                </div>
-            </Paper>
-        )
+        mainEl = <div/>
     }
+
+
+    const landingPage = (
+        <Paper className={classes.root} square={true}>
+            <div className={classes.mainPage}
+                 style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div style={{
+                    display: 'flex',
+                    width: '100%',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    padding: '10px'
+                }}>
+                    <h1 className={classes.logo} style={{
+                        fontSize: '1em',
+                        letterSpacing: '1px',
+                        fontWeight: '500',
+                        margin: '0px',
+                        marginTop: '3px'
+                    }}>How does Flexboard Work?</h1>
+                    <h1 className={classes.logo} style={{
+                        fontSize: '1em',
+                        letterSpacing: '1px',
+                        fontWeight: '500',
+                        margin: '0px',
+                        marginTop: '3px',
+                        justifyContent: 'center'
+                    }}>About Us</h1>
+                </div>
+
+                <h6 className={classes.logo}>FLEXBOARD</h6>
+
+                <h1 className={classes.logo} style={{
+                    fontSize: '1.5em',
+                    letterSpacing: '1px',
+                    fontWeight: '100',
+                    margin: '0px',
+                    marginTop: '5px'
+                }}>
+                    Powering Enterprise Models to Life
+                    <sup style={{fontSize: '0.7em', marginLeft: '5px'}}>&trade;</sup>
+                </h1>
+                <Button
+                    elevation={50}
+                    style={{
+                        marginTop: '50px',
+                        backgroundColor: '#A5014B',
+                        fontFamily: 'Questrial',
+                        fontSize: '1em',
+                        width: '300px',
+                        color: '#E7F1F6'
+                    }}
+                    onClick={() => loginWithRedirect({
+                        redirect_uri: 'http://localhost:3000/home',
+                        // audience:'http://localhost/5000',
+                        // scope:'read:records'
+                    })}>
+                    Secure Log in
+                </Button>
+                <Button
+                    elevation={50}
+                    style={{
+                        marginTop: '50px',
+                        backgroundColor: '#006E9F',
+                        fontFamily: 'Questrial',
+                        fontSize: '1em',
+                        width: '300px',
+                        color: '#E7F1F6'
+                    }}>Create an Account</Button>
+            </div>
+        </Paper>
+    )
+
 
     return (
         <div>
-            {mainEl}
+            <Switch>
+                <Route exact path={"/"}>
+                    {landingPage}
+                </Route>
+                <Route exact
+                       path="/(home|summary|dashboard|distributions|inputimportance|sensitivity|scenario|spreadsheet)">
+                    {mainEl}
+                </Route>
+            </Switch>
         </div>
     )
 }
